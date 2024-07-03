a0b2aaadf53f5d5494110dba48d8036de3098f0a
Mukhammad <muxammadhotamo@gmail.com>
2024-07-03 19:40:06 +0500

    8. New Logic for Teilstatus OK.

--- lib/global/index.dart
+++ lib/global/index.dart
@@ -4,8 +4,8 @@ String key = '';
 // String key = '';
 // 04711227280
 String user = '';
-// String ipAdress = '192.168.100.23:7878';
-String ipAdress = '104.198.75.202';
+String ipAdress = '192.168.100.23:7878';
+// String ipAdress = '104.198.75.202';
 // String ipAdress = '127.0.0.1:5000';
 int count = 0;
 bool showAlert = false;
